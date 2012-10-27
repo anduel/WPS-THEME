b38c9be3614059dbb1335cbe2c63446ea943c2a6
Anduel Mehmeti <anduel.mehmeti@gmail.com>
2012-10-27 18:32:24 +0200

    WPS-Theme

---
+++ home.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<head>
+<title></title>
+<link type="text/css" href="style.css" rel="stylesheet" media="screen"/> 
+</head>
+<body>
+
+<!-- CONTAINER -->
+
+<div id="container"> <!-- div container -->
+
+<!-- HEADER -->
+
+	<div id="header"> <!-- div header -->
+		<div id="first-bar"> <!-- div first-bar -->
+			<div id="left-first-bar"> <!-- div left-first-bar -->
+				<a href="http://#"> <div id="logo-home"> <!-- div logo-home -->
+				</div> </a> <!-- end of div logo-home -->
+				<a href="http://#"> <div id="logo-network"> <!-- div logo-network -->
+				</div> </a> <!-- end of logo-network -->
+				<a href="http://#"> <div id="logo-message"> <!-- div logo-message -->
+				</div> </a> <!-- end of div logo-message -->
+			</div> <!-- end of div left-first-bar -->
+			<div id="right-first-bar"> <!-- div right-first-bar -->
+				<div id="left-rfb"> <!-- div left-rfb -->
+					<h2>Need Help? Please email or call </h2>
+				</div> <!-- end of left-rfb -->
+				<div id="right-rfb"> <!-- div right-rfb -->
+					<h2 id="right-rfb">1-800-345-5894 </h2> 
+				</div> <!-- end of div right-rfb -->
+			</div> <!-- end of div right-first-bar -->
+		</div> <!-- end of  div first-bar -->
+		<div id="header-container"> <!-- div header-container -->
+			<div id="menu-logo-container">
+				<div id="logo"> <!-- div logo  -->
+				</div> <!-- end of div logo -->
+				<div id="menu"> <!-- div menu -->
+					<ul>
+						<li class="menu-first-item other-menu-items"> <a href="#">Home</a> </li>
+						<li class="other-menu-items"> <a href="#">Hosting</a> </li>
+						<li class="other-menu-items"> <a href="#">Features </a> </li>
+						<li class="other-menu-items"> <a href="#">About </a> </li>
+						<li class="other-menu-items"> <a href="#">Support </a> </li>
+						<li class="other-menu-items"> <a href="#">Contacts</a> </li>
+					</ul>
+				</div> <!-- end of div menu -->
+			</div> <!-- end of menu-logo-container -->
+			<div id="left-header-container"> <!-- div left-header-container -->
+				<div id="top-left-header-container"> <!-- div top-left-header-container -->
+					<h1>Simple Clever Effective </h1>
+				</div> <!-- end of div top-left-header-container -->
+				<div id="bottom-left-header-container"> <!-- div bottom-left-header-container -->
+					<h2 class="first">HOSTINGWEBSITETEMPLATE </h2>
+					<h2 class="second">BYTEMPLATEMONSTER.COM</h2>
+				</div> <!-- end of div bottom-left-header-container -->
+			</div> <!-- end of div left-header-container -->
+			<div id="right-header-container"> <!-- div right-header-container -->
+			</div> <!-- end of div right-header-container -->
+
+		</div> <!-- end of div header-container -->
+	</div> <!-- end of div header -->
+	
+<!-- end of HEADER -->
+
+<!-- CONTENT -->
+
+	<div id="content"> <!-- div content -->
+		<div id="content-1"> <!-- div content-1 -->
+			<div id="left-content-1"> <!-- div left-content-1 -->
+				<div id="top-left-content-1"> <!-- div top-left-content-1 -->
+					<div id="textarea-tlc-1"> <!-- div textarea-tlc-1 -->
+						<h1>Small</h1>
+						<h2 class="format-txt1">$9.95/month</h2>
+					</div> <!-- end of div textarea-tlc-1 -->
+				</div> <!-- end of div top-left-content-1 --> 
+				<div id="center-left-content-1"> <!-- div center-left-content-1 -->
+					<div id="textarea-clc-1"> <!-- div textarea-clc-1 -->
+						<div class="row1"> <!-- div row1 --> 
+							<h3 class="left">Disk space</h3> 
+							<h3 class="right">300 GB</h3>
+						</div> <!-- end of div row1 -->
+						<div class="row2"> <!-- div row2 -->
+							<h3 class="left">Monthly data transfer</h3> 
+							<h3 class="right">3000 GB</h3>
+						</div> <!-- end of div row2 -->
+						<div class="row3"> <!-- div row3 -->
+							<h3 class="left">FTP accounts</h3> 
+							<h3 class="right">25</h3>
+						</div> <!-- end of div row3 -->
+						<div class="row4"> <!-- div row4 -->
+							<h3 class="left">Email boxes</h3> 
+							<h3 class="right">1000</h3>
+						</div> <!-- end of div row4 -->
+						<div class="row5"> <!-- div row5 -->
+							<h3 class="left">Free domains</h3> 
+							<h3 class="right">1</h3>
+						</div> <!-- end of div row5 -->
+					</div> <!-- end of div textarea-clc-1 -->  
+				</div> <!-- end of div center-left-content-1 -->
+				<div id="bottom-left-content-1"> <!-- div bottom-left-content-1 -->
+					<div class="details-bay"> <!-- div details-bay -->
+						<a href="http://#"> <div id="details1"> <!-- div details1 -->
+						</div> </a> <!-- end of div details1 -->
+						<a href="http://#"> <div id="bay1"> <!-- div bay1 -->
+						</div> </a> <!-- end of div bay1 -->
+					</div> <!-- end of details-bay -->
+				</div> <!-- end of div bottom-left-content-1 -->
+			</div> <!-- end of div left-content-1 -->
+			<div id="center-content-1"> <!-- div center-content-1 -->
+				<div id="top-center-content-1"> <!-- div top-center-content-1 -->
+					<div id="textarea-tcc-1"> <!-- div textarea-tcc-1 -->
+						<h1>Medium</h1>
+						<h2 class="format-txt1">$14.95/month</h2>
+					</div> <!-- end of div textarea-tcc-1 -->
+				</div> <!-- end of div top-center-content-1 -->
+				<div id="center-center-content-1"> <!-- div center-center-content-1 -->
+					<div id="textarea-ccc-1"> <!-- div textarea-ccc-1 -->
+						<div class="row1"> <!-- div row1 --> 
+							<h3 class="left">Disk space</h3> 
+							<h3 class="right">500 GB</h3>
+						</div> <!-- end of div row1 -->
+						<div class="row2"> <!-- div row2 -->
+							<h3 class="left">Monthly data transfer</h3> 
+							<h3 class="right">5000 GB</h3>
+						</div> <!-- end of div row2 -->
+						<div class="row3"> <!-- div row3 -->
+							<h3 class="left">FTP accounts</h3> 
+							<h3 class="right">50</h3>
+						</div> <!-- end of div row3 -->
+						<div class="row4"> <!-- div row4 -->
+							<h3 class="left">Email boxes</h3> 
+							<h3 class="right">1000</h3>
+						</div> <!-- end of div row4 -->
+						<div class="row5"> <!-- div row5 -->
+							<h3 class="left">Free domains</h3> 
+							<h3 class="right">1</h3>
+						</div> <!-- end of div row5 -->
+					</div> <!-- end of div textarea-ccc-1 --> 
+				</div> <!-- end of div center-center-content-1 -->
+				<div id="bottom-center-content-1"> <!-- div bottom-center-content-1 -->
+					<div class="details-bay"> <!-- div details-bay -->
+						<a href="http://#"> <div id="details2"> <!-- div details1 -->
+						</div> </a> <!-- end of div details1 -->
+						<a href="http://#"> <div id="bay2"> <!-- div bay1 -->
+						</div> </a> <!-- end of div bay1 -->
+					</div> <!-- end of details-bay -->
+				</div> <!-- end of div bottom-center-content-1 -->
+			</div> <!-- end of div center-content-1 -->
+			<div id="right-content-1"> <!-- div right-content-1 -->
+				<div id="top-right-content-1"> <!-- div top-right-content-1 -->
+					<div id="textarea-trc-1"> <!-- div textarea-trc-1 -->
+						<h1>Unlimited</h1>
+						<h2 class="format-txt1">$19.95/month</h2>
+					</div> <!-- end of div textarea-trc-1 -->
+				</div> <!-- end of div top-right-content-1 -->
+				<div id="center-right-content-1"> <!-- div center-right-content-1 -->
+					<div id="textarea-crc-1"> <!-- div textarea-crc-1 -->
+						<div class="row1"> <!-- div row1 --> 
+							<h3 class="left">Disk space</h3> 
+							<h3 class="right">Unlimited</h3>
+						</div> <!-- end of div row1 -->
+						<div class="row2"> <!-- div row2 -->
+							<h3 class="left">Monthly data transfer</h3> 
+							<h3 class="right">Unlimited</h3>
+						</div> <!-- end of div row2 -->
+						<div class="row3"> <!-- div row3 -->
+							<h3 class="left">FTP accounts</h3> 
+							<h3 class="right">Unlimited</h3>
+						</div> <!-- end of div row3 -->
+						<div class="row4"> <!-- div row4 -->
+							<h3 class="left">Email boxes</h3> 
+							<h3 class="right">Unlimited</h3>
+						</div> <!-- end of div row4 -->
+						<div class="row5"> <!-- div row5 -->
+							<h3 class="left">Free domains</h3> 
+							<h3 class="right">3</h3>
+						</div> <!-- end of div row5 -->
+					</div> <!-- end of div textarea-crc-1 --> 
+				</div> <!--end of div center-right-content-1 -->
+				<div id="bottom-right-content-1"> <!-- div bottom-right-content-1 -->
+					<div class="details-bay"> <!-- div details-bay -->
+						<a href="http://#"> <div id="details3"> <!-- div details1 -->
+						</div> </a> <!-- end of div details1 -->
+						<a href="http://#"> <div id="bay3"> <!-- div bay1 -->
+						</div> </a> <!-- end of div bay1 -->
+					</div> <!-- end of details-bay -->
+				</div> <!-- end of div bottom-right-content-1 -->
+			</div> <!-- end of div right-content-1 -->
+		</div> <!--end of div content-1 -->
+		<div id="content-2"> <!-- div content-2 -->
+			<div id="left-content-2"> <!-- div left-content-2 -->
+				<div id="top-left-content-2"> <!-- div top-left-content-2 -->
+					<h1>Register Domain Name </h1>
+				</div> <!-- end of div top-left-content-2 -->
+				<div id="center-left-content-2"> <!-- div center-left-content-2 -->
+					<div id="left-center-left-content-2"> <!-- div left-center-left-content-2 -->
+						<form class="search-box"> <!-- search form -->
+							<input name="search" type="text" id="search" value="Type domain name here">
+						</form> <!-- end of search-form -->
+					</div> <!-- end of div left-center-left-content-2 -->
+					<div id="right-center-left-content-2"> <!-- div right-center-left-content-2 -->
+						<div id="col1-right-center-left-content-2"> <!-- div col1-right-center-left-content-2 -->
+							<form class="chck-left" >
+								<input type="checkbox" name="net" value="net">.net<br>
+								<input type="checkbox" name="info" value="info">.info<br>
+								<input type="checkbox" name="co.uk" value="co.uk">.co.uk<br>
+								<input type="checkbox" name="mx" value="mx">.mx
+							</form >
+						</div> <!-- end of div col1-right-center-left-content-2 -->
+						<div id="col2-right-center-left-content-2"> <!-- div col2-right-center-left-content-2 -->
+							<form class="chck-right" >
+								<input type="checkbox" name="com" value="com">.com<br>
+								<input type="checkbox" name="mobi" value="mobi">.mobi<br>
+								<input type="checkbox" name="eu" value="eu">.eu<br>
+								<input type="checkbox" name="us" value="us">.us
+							</form >
+						</div> <!-- end of div col2-right-center-left-content-2 -->
+						<div id="col3-right-center-left-content-2"> <!-- div col3-right-center-left-content-2 -->
+							<form class="chck-left" >
+								<input type="checkbox" name="net" value="net">.net<br>
+								<input type="checkbox" name="info" value="info">.info<br>
+								<input type="checkbox" name="co.uk" value="co.uk">.co.uk<br>
+								<input type="checkbox" name="mx" value="mx">.mx
+							</form >
+						</div> <!-- end of div col3-right-center-left-content-2 -->
+						<div id="col4-right-center-left-content-2"> <!-- div col4-right-center-left-content-2 -->
+							<form class="chck-right" >
+								<input type="checkbox" name="com" value="com">.com<br>
+								<input type="checkbox" name="mobi" value="mobi">.mobi<br>
+								<input type="checkbox" name="eu" value="eu">.eu<br>
+								<input type="checkbox" name="us" value="us">.us
+							</form >
+						</div> <!-- end of div col4-right-center-left-content-2 -->
+					</div> <!-- end of div right-center-left-content-2 -->
+				</div> <!-- end of div center-left-content-2 -->
+				<div id="bottom-left-content-2"> <!-- div bottom-left-content-2 -->
+					<div id="left-bottom-left-content-2"> <!-- div left-bottom-left-content-2 -->
+						<ul> 
+							<a href="http://#"> <li class="first-item">Renew a domain</li> </a>
+							<a href="http://#"> <li class="other-items">Transfer a domain</li> </a>
+							<a href="http://#"> <li class="other-items">WHOIS</li> </a>
+						</ul>
+					</div> <!-- end of div left-bottom-left-content-2 -->
+					<div id="right-bottom-left-content-2"> <!-- div right-bottom-left-content-2 -->
+						<a href="http://#"> <div id="check-domain"> <!-- div check-domain -->
+						</div> </a> <!-- end of div check-domain -->
+					</div> <!-- end of div right-bottom-left-content-2 -->
+				</div> <!-- end of div bottom-left-content-2 -->
+			</div> <!-- end of div left-content-2 -->
+			<div id="right-content-2"> <!-- div right-content-2 -->
+				<div id="top-right-content-2"> <!-- div top-right-content-2 -->
+					<h1>Get Started!</h1>
+				</div> <!-- end of div to<a href="http://#">p-right-content-2 -->
+				<div id="bottom-right-content-2"> <!-- div bottom-right-content-2 -->
+					<div id="textarea2">	<!-- div textarea2 -->			
+						<h3>Don't have hosting website yet ?</h3>
+						<p> Let us help you get started offer you this theme -one off <a href="http://#">free website templates</a>
+						created by TemplateMonster.com . This template goes with two packages - with PSD source files are available 
+						for free for the registered members of </p>
+					</div> <!-- end of div textarea2 -->	
+				</div> <!-- end of div bottom-right-content-2 -->
+			</div> <!-- end of div right-content-2 -->
+		</div> <!-- end of div content-2 -->
+	</div> <!-- end of div content -->
+	
+<!-- end of CONTENT -->
+
+<!-- FOOTER -->
+
+	<div id="footer"> <!-- div footer -->
+		<div id="footer-container"> <!-- div footer-container -->
+		<h3 id="h3-footer-1"> <a href="http://#">Website template</a> designed by TemplateMonster.com</h3>
+		<h3 id="h3-footer-2"> <a href="http://#">3D Models</a> provided by Templates.com</h3>
+		</div> <!-- end of div footer-container -->
+	</div> <!-- end of div footer -->
+	
+<!-- end of FOOTER -->
+
+</div> <!-- end of div container -->
+
+<!-- end of CONTAINER -->
+
+</body>
+</html>
