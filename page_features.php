5a34a80b444a1668c8368e29257d8a142ce144e1
Anduel Mehmeti <anduel.mehmeti@gmail.com>
2012-10-30 23:04:55 +0100

    created page_features

---
+++ page_features.php
@@ -0,0 +1,384 @@
+<?php get_header();
+
+/**
+ * Template Name: Features
+ */
+?>
+
+<!-- CONTENT -->
+
+	<div id="content-features"> <!-- div content -->
+		<div id="content-1-features"> <!-- div content-1 -->
+		 <div id="content-1-features-container"> <!-- div content-1-features-container -->
+		 	<div id="top-content-1-features-container"> <!-- div top-content-1-features-container -->
+		 		<h2>Medium Hosting Plan Features</h2>
+		 	</div> <!-- end of div top-content-1-features-container --> 
+		 	<div id="center-content-1-features-container"> <!-- div center-content-1-features-container -->
+		 		<p>Lorem ipsum dolor sit amet,consectetur adispicing elit.Mauris tempor , justo sodales fermentum consectetur, libero tellus
+		 		 fermentum feugiat molestie orci enim sit amet libero.Vivamus non velit in nibh tincidunt leo. Phasellus eget nulla odio.Maecenas 
+		 		 gravida lorem integer imperdiet euismod neque quis luctus. </p>
+		 	</div> <!-- end of div center-content-1-features-container --> 
+		 	<div id="bottom-content-1-features-container"> <!-- div bottom-content-1-features-container -->
+		 		<div id="left-bottom-content-1-features-container"> <!-- div left-bottom-content-1-features-container -->
+		 			<div id="top-left-bottom-content-1-features-container"> <!-- div top-left-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>500 GB Disk Space</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div top-left-bottom-content-1-features-container -->
+		 			<div id="center-left-bottom-content-1-features-container"> <!-- div center-left-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>Unlimited Data Transfer</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div center-left-bottom-content-1-features-container -->
+		 			<div id="bottom-left-bottom-content-1-features-container"> <!-- div bottom-left-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>SSL,FTP,Stats</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div bottom-left-bottom-content-1-features-container -->
+		 		</div> <!-- end of div left-bottom-content-1-features-container -->
+		 		<div id="center-bottom-content-1-features-container"> <!-- div center-bottom-content-1-features-container -->
+		 			<div id="top-center-bottom-content-1-features-container"> <!-- div top-center-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>1000 Email Accounts</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div top-center-bottom-content-1-features-container -->
+		 			<div id="center-center-bottom-content-1-features-container"> <!-- div center-center-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>Free Domain Name</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div center-center-bottom-content-1-features-container -->
+		 			<div id="bottom-center-bottom-content-1-features-container"> <!-- div bottom-center-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>Free Site Builder</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div bottom-center-bottom-content-1-features-container -->
+		 		</div> <!-- end of div center-bottom-content-1-features-container -->
+		 		<div id="right-bottom-content-1-features-container"> <!-- div right-bottom-content-1-features-container -->
+		 			<div id="top-right-bottom-content-1-features-container"> <!-- div top-right-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>Best Support in the industry</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div top-right-bottom-content-1-features-container -->
+		 			<div id="center-right-bottom-content-1-features-container"> <!-- div center-right-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>Unlimited Domain Hosting</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div center-right-bottom-content-1-features-container -->
+		 			<div id="bottom-right-bottom-content-1-features-container"> <!-- div bottom-right-bottom-content-1-features-container -->
+		 				<div class="shortcut"> <!-- div shortcut -->
+		 				</div> <!-- end of div shortcut -->
+		 				<div class="text"> <!-- div text -->
+		 					<h2>CGI,Ruby(RoR),Perl,PHP,MySQL</h2>
+		 				</div> <!-- end of div text -->
+		 			</div> <!-- end of div bottom-right-bottom-content-1-features-container -->
+		 		</div> <!-- end of div right-bottom-content-1-features-container -->
+		 	</div> <!-- end of div bottom-content-1-features-container --> 
+		 </div> <!-- end of content-1-features-container -->
+		</div> <!-- end of div content-1-features -->
+		<div id="content-2-features"> <!-- div content-2-features -->
+			<div id="content-2-features-container"> <!-- div content-2-features-container -->
+				<div id="left-content-2-features-container"> <!-- div left-content-2-features-container -->
+					<div class="left-class"> <!-- div left-class -->
+						<div class="top-left-class-"> <!-- div top-left-class -->
+							<div class="first-top-lr-class"> <!-- div first-top-lr-class -->
+								<h2>FeaturesDetails</h2>
+							</div> <!-- end of div first-top-lr-class -->
+							<div class="txt-container"> <!-- div txt-container -->
+								<div class="txt1"> <!-- div txt1 -->
+									<h2>GB of Disk Space</h2>
+								</div> <!-- end of div txt1 -->
+								<div class="txt2"> <!-- div txt2 -->
+									<h2>Hosted Domains Allowed</h2>
+								</div> <!-- end of div txt2 -->
+								<div class="txt3"> <!-- div txt3 -->
+									<h2>Free Domain Name</h2>
+								</div> <!-- end of div txt3 -->
+								<div class="txt4"> <!-- div txt4 -->
+									<h2>Instant Activation</h2>
+								</div> <!-- end of div txt4 -->
+								<div class="txt5"> <!-- div txt5 -->
+									<h2>International Domain Names</h2>
+								</div> <!-- end of div txt5 -->
+								<div class="txt6"> <!-- div txt6 -->
+									<h2>POP3/POP3 Secure Email Accounts</h2>
+								</div> <!-- end of div txt6 -->
+								<div class="txt7"> <!-- div txt7 -->
+									<h2>IMAP/Secure IMAP Email Accounts</h2>
+								</div> <!-- end of div txt7 -->
+								<div class="txt8"> <!-- div txt8 -->
+									<h2>GB's of Site Transfer (Bandwidth)</h2>
+								</div> <!-- end of div txt8 -->
+							</div> <!-- end of div txt-container -->
+						</div> <!-- end of div top-left-class -->
+						<div class="bottom-left-class"> <!-- div bottom-left-class -->
+							<div class="first-top-lr-class"> <!-- div first-top-lr-class -->
+								<h2>e-Commerce FeaturesDetails</h2>
+							</div> <!-- end of div first-top-lr-class -->
+							<div class="txt-container"> <!-- div txt-container -->
+								<div class="txt1"> <!-- div txt1 -->
+									<h2>SSL Secure Server</h2>
+								</div> <!-- end of div txt1 -->
+								<div class="txt2"> <!-- div txt2 -->
+									<h2>OS Commerce Shopping Cart</h2>
+								</div> <!-- end of div txt2 -->
+								<div class="txt3"> <!-- div txt3 -->
+									<h2>Agora Shopping Cart</h2>
+								</div> <!-- end of div txt3 -->
+								<div class="txt4"> <!-- div txt4 -->
+									<h2>Free Generated Certificate</h2>
+								</div> <!-- end of div txt4 -->
+								<div class="txt5"> <!-- div txt5 -->
+									<h2>Password Protected Directories</h2>
+								</div> <!-- end of div txt5 -->
+								<div class="txt6"> <!-- div txt6 -->
+									<h2>Open PGP/GPG Encryption</h2>
+								</div> <!-- end of div txt6 -->
+								<div class="txt7"> <!-- div txt7 -->
+									<h2>Paypal Support</h2>
+								</div> <!-- end of div txt7 -->
+								<div class="txt8"> <!-- div txt8 -->
+									<h2>Merchant Account Support</h2>
+								</div> <!-- end of div txt8 -->
+							</div> <!-- end of div txt-container -->
+						</div> <!-- end of div bottom-left-class -->
+					</div> <!-- end of div left-class -->
+					<div class="right-class"> <!-- div right-class -->
+						<div class="top-right-class"> <!-- div top-right-class -->
+							<div class="first-top-lr-class-2"> <!-- div first-top-lr-class-2 -->
+							</div> <!-- end of div first-top-lr-class-2 -->
+							<div class="txt-container-2"> <!-- div txt-container-2 -->
+								<div class="txt1-2"> <!-- div txt1-2 -->
+									<h2>Unlimited</h2>
+								</div> <!-- end of div txt1-2 -->
+								<div class="txt2-2"> <!-- div txt2-2 -->
+									<h2>Unlimited</h2>
+								</div> <!-- end of div txt2-2 -->
+								<div class="txt3-2"> <!-- div txt3-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt3-2 -->
+								<div class="txt4-2"> <!-- div txt4-2 -->
+									<h2>Included FREE</h2>
+								</div> <!-- end of div txt4-2 -->
+								<div class="txt5-2"> <!-- div txt5-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt5-2 -->
+								<div class="txt6-2"> <!-- div txt6-2 -->
+									<h2>Unlimited</h2>
+								</div> <!-- end of div txt6-2 -->
+								<div class="txt7-2"> <!-- div txt7-2 -->
+									<h2>Unlimited</h2>
+								</div> <!-- end of div txt7-2 -->
+								<div class="txt8-2"> <!-- div txt8-2 -->
+									<h2>Unlimited</h2>
+								</div> <!-- end of div txt8-2 -->
+							</div> <!-- end of div txt-container-2 -->
+						</div> <!-- end of div top-right-class -->
+						<div class="bottom-right-class"> <!-- div bottom-right-class -->
+							<div class="first-top-lr-class-2"> <!-- div first-top-lr-class-2 -->
+							</div> <!-- end of div first-top-lr-class-2 -->
+							<div class="txt-container-2"> <!-- div txt-container-2 -->
+								<div class="txt1-2"> <!-- div txt1-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt1-2 -->
+								<div class="txt2-2"> <!-- div txt2-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt2-2 -->
+								<div class="txt3-2"> <!-- div txt3-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt3-2 -->
+								<div class="txt4-2"> <!-- div txt4-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt4-2 -->
+								<div class="txt5-2"> <!-- div txt5-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt5-2 -->
+								<div class="txt6-2"> <!-- div txt6-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt6-2 -->
+								<div class="txt7-2"> <!-- div txt7-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt7-2 -->
+								<div class="txt8-2"> <!-- div txt8-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt8-2 -->
+							</div> <!-- end of div txt-container-2 -->
+						</div> <!-- end of div bottom-right-class -->
+					</div> <!-- end of div right-class -->
+				</div> <!-- end of div left-content-2-features-container -->
+				<div id="right-content-2-features-container"> <!-- div right-content-2-features-container -->
+					<div class="left-class"> <!-- div left-class -->
+						<div class="top-left-class"> <!-- div top-left-class -->
+							<div class="first-top-lr-class"> <!-- div first-top-lr-class -->
+								<h2>Bonus Features</h2>
+							</div> <!-- end of div first-top-lr-class -->
+							<div class="txt-container"> <!-- div txt-container -->
+								<div class="txt1"> <!-- div txt1 -->
+									<h2>Live Support in cPanel</h2>
+								</div> <!-- end of div txt1 -->
+								<div class="txt2"> <!-- div txt2 -->
+									<h2>Bulletin Board (Message Forum)</h2>
+								</div> <!-- end of div txt2 -->
+								<div class="txt3"> <!-- div txt3 -->
+									<h2>Form mail Script</h2>
+								</div> <!-- end of div txt3 -->
+								<div class="txt4"> <!-- div txt4 -->
+									<h2>PHP Nuke</h2>
+								</div> <!-- end of div txt4 -->
+								<div class="txt5"> <!-- div txt5 -->
+									<h2>1 click WordPress Install</h2>
+								</div> <!-- end of div txt5 -->
+								<div class="txt6"> <!-- div txt6 -->
+									<h2>1 click b2evolution Install</h2>
+								</div> <!-- end of div txt6 -->
+								<div class="txt7"> <!-- div txt7 -->
+									<h2>1 click Joomla Install</h2>
+								</div> <!-- end of div txt7 -->
+								<div class="txt8"> <!-- div txt8 -->
+									<h2>Web Blogs/WordPress/b2evolution</h2>
+								</div> <!-- end of div txt8 -->
+							</div> <!-- end of div txt-container -->
+						</div> <!-- end of div top-left-class -->
+						<div class="bottom-left-class"> <!-- div bottom-left-class -->
+							<div class="first-top-lr-class"> <!-- div first-top-lr-class -->
+								<h2>Technology</h2>
+							</div> <!-- end of div first-top-lr-class -->
+							<div class="txt-container"> <!-- div txt-container -->
+								<div class="txt1"> <!-- div txt1 -->
+									<h2>Quad Processor Servers</h2>
+								</div> <!-- end of div txt1 -->
+								<div class="txt2"> <!-- div txt2 -->
+									<h2>Linux Operating System</h2>
+								</div> <!-- end of div txt2 -->
+								<div class="txt3"> <!-- div txt3 -->
+									<h2>Customized Apache Web Server</h2>
+								</div> <!-- end of div txt3 -->
+								<div class="txt4"> <!-- div txt4 -->
+									<h2>UPS Power Back-up Generator </h2>
+								</div> <!-- end of div txt4 -->
+								<div class="txt5"> <!-- div txt5 -->
+									<h2>24/7 Monitoring</h2>
+								</div> <!-- end of div txt5 -->
+								<div class="txt6"> <!-- div txt6 -->
+									<h2>Courtesy Site Backups</h2>
+								</div> <!-- end of div txt6 -->
+								<div class="txt7"> <!-- div txt7 -->
+									<h2>OC-96 Backbone Connection</h2>
+								</div> <!-- end of div txt7 -->
+								<div class="txt8"> <!-- div txt8 -->
+									<h2>400 Gigabits Per SecondSelf-signed SSL</h2>
+								</div> <!-- end of div txt8 -->
+							</div> <!-- end of div txt-container -->
+						</div> <!-- end of div bottom-left-class -->
+					</div> <!-- end of div left-class -->
+					<div class="right-class"> <!-- div right-class -->
+						<div class="top-right-class"> <!-- div top-right-class -->
+							<div class="first-top-lr-class-2"> <!-- div first-top-lr-class-2 -->
+							</div> <!-- end of div first-top-lr-class-2 -->
+							<div class="txt-container-2"> <!-- div txt-container-2 -->
+								<div class="txt1-2"> <!-- div txt1-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt1-2 -->
+								<div class="txt2-2"> <!-- div txt2-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt2-2 -->
+								<div class="txt3-2"> <!-- div txt3-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt3-2 -->
+								<div class="txt4-2"> <!-- div txt4-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt4-2 -->
+								<div class="txt5-2"> <!-- div txt5-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt5-2 -->
+								<div class="txt6-2"> <!-- div txt6-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt6-2 -->
+								<div class="txt7-2"> <!-- div txt7-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt7-2 -->
+								<div class="txt8-2"> <!-- div txt8-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt8-2 -->
+							</div> <!-- end of div txt-container-2 -->	
+						</div> <!-- end of div top-right-class -->
+						<div class="bottom-right-class"> <!-- div bottom-right-class -->
+							<div class="first-top-lr-class-2"> <!-- div first-top-lr-class-2 -->
+							</div> <!-- end of div first-top-lr-class-2 -->
+							<div class="txt-container-2"> <!-- div txt-container-2 -->
+								<div class="txt1-2"> <!-- div txt1-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt1-2 -->
+								<div class="txt2-2"> <!-- div txt2-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt2-2 -->
+								<div class="txt3-2"> <!-- div txt3-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt3-2 -->
+								<div class="txt4-2"> <!-- div txt4-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt4-2 -->
+								<div class="txt5-2"> <!-- div txt5-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt5-2 -->
+								<div class="txt6-2"> <!-- div txt6-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt6-2 -->
+								<div class="txt7-2"> <!-- div txt7-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt7-2 -->
+								<div class="txt8-2"> <!-- div txt8-2 -->
+									<div class="ok"> <!-- div ok -->
+									</div> <!-- end of div ok -->
+								</div> <!-- end of div txt8-2 -->
+							</div> <!-- end of div txt-container-2 -->
+						</div> <!-- end of div bottom-right-class -->
+					</div> <!-- end of div right-class -->
+				</div> <!-- end of div right-content-2-features-container -->
+			</div> <!-- end of div content-2-features-container -->
+		</div> <!-- end of div content-2-features -->
+	</div> <!-- end of div content-features -->
+	
+<!-- end of CONTENT -->
+
+<?php get_footer(); ?>
