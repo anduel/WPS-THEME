e0ba2bfd07fb38a64a8b24eaadef36f7f22bac47
Anduel Mehmeti <anduel.mehmeti@gmail.com>
2012-10-31 18:49:14 +0100

    created page_about

---
+++ page_about.php
@@ -0,0 +1,133 @@
+<?php get_header();
+
+/**
+ * Template Name: About
+ */
+?>
+
+<!-- CONTENT -->
+
+	<div id="content-about"> <!-- div content-about -->
+		<div id="content-1-about"> <!-- div content-1-about -->
+		 <div id="content-1-about-container"> <!-- div content-1-about-container -->
+		 	<div id="top-content-1-about-container"> <!-- div top-content-1-about-container -->
+		 		<h2>A Few Words About Us</h2>
+				</div> <!-- end of div top-content-1-about-container -->
+				<div id="center-content-1-about-container"> <!-- div center-content-1-about-container -->
+					<div id="img-center-content-1-about-container"> <!-- div img-center-content-1-about-container -->
+					</div> <!-- end of div img-center-content-1-about-container -->
+					<div id="tekst-center-content-1-about-container">  <!-- div tekst-center-content-1-about-container -->
+						<p>amus non velit in nibh tincidunt tristique tincidunt in leo.Phasellus eget nulla odio.Maecenas at gravida
+						em. Integer imperdiet euismod neque quis luctus.Donec facilis lobortis viverra.Quisque vehicula susit pouere.
+						Nam non lacus dui , gravida conidimentum tortor.Maecenas ultrices volutpat euismod.Nam lentesque imperdiet urna
+						,quis adipiscing nunc blandit non.Maecenas imperdiet ultricies dolor curcus cumsan at sem non lacus euismod aliquet 
+						in nec nisl.  </p>
+					</div> <!-- end of div tekst-center-content-1-about-container -->
+				</div> <!-- end of div center-content-1-about-container -->
+				<div id="bottom-content-1-about-container"> <!-- div bottom-content-1-about-container -->
+					<p>amus non velit in nibh tincidunt tristique tincidunt in leo.Phasellus eget nulla odio.Maecenas at gravida
+						em. Integer imperdiet euismod neque quis luctus.Donec facilis lobortis viverra.Quisque vehicula susit pouere.
+						Nam non lacus dui , gravida conidimentum tortor.Maecenas ultrices volutpat euismod.Nam lentesque imperdiet urna
+						,quis adipiscing nunc blandit non.Maecenas imperdiet ultricies dolor curcus cumsan at sem non lacus euismod aliquet 
+						in nec nisl.  </p>
+				</div> <!-- end of div bottom-content-1-about-container -->
+		 </div> <!-- end of content-1-about-container -->
+		</div> <!-- end of div content-1-about -->
+		<div id="content-2-about"> <!-- div content-2-about -->
+			<div id="content-2-about-container"> <!-- div content-2-about-container -->
+				<div id="left-content-2-about-container"> <!-- div left-content-2-about-container -->
+					<div id="first-left-content-2-about-container"> <!-- div first-left-content-2-about-container -->
+						<h2>Why Us</h2>
+					</div> <!-- end of div first-left-content-2-about-container -->
+					<div id="second-left-content-2-about-container"> <!-- div second-left-content-2-about-container -->
+						<p>Lorem ipsum dolor sit amet,consectetur adispicing elit.Mauris tempor , justo sodales fermentum consectetur, libero tellus
+		 		 			fermentum feugiat molestie orci enim sit amet libero.Vivamus non velit in nibh tincidunt leo. Phasellus eget nulla odio.Maecenas 
+		 					 gravida lorem integer imperdiet euismod neque quis luctus. </p>
+					</div> <!-- end of div second-left-content-2-about-container -->
+					<div id="third-left-content-2-about-container"> <!-- div third-left-content-2-about-container -->
+						<div id="text-container-left-about-1"> <!-- div text-container-left-about-1 -->
+							<div class="icon"> <!-- div icon -->
+							</div> <!-- end of div icon -->
+							<div class="text-left-about"> <!-- div text-left-about -->
+								<h2>Nam non lacus dui, gravida condimentum tortor</h2>
+							</div> <!-- end of div text-left-about --> 
+						</div> <!-- end of div text-container-left-about-1 -->
+						<div id="text-container-left-about-2"> <!-- div text-container-left-about-2 -->
+							<div class="icon"> <!-- div icon -->
+							</div> <!-- end of div icon -->
+							<div class="text-left-about"> <!-- div text-left-about -->
+								<h2>Maecenas ultrices volutpat euismod</h2>
+							</div> <!-- end of div text-left-about --> 
+						</div> <!-- end of div text-container-left-about-2 -->
+						<div id="text-container-left-about-3"> <!-- div text-container-left-about-3 -->
+							<div class="icon"> <!-- div icon -->
+							</div> <!-- end of div icon -->
+							<div class="text-left-about"> <!-- div text-left-about -->
+								<h2>Nam pellentesque imperdiet urna, quis adipiscing nunc blandit non </h2>
+							</div> <!-- end of div text-left-about --> 
+						</div> <!-- end of div text-container-left-about-3 -->
+						<div id="text-container-left-about-4"> <!-- div text-container-left-about-4 -->
+							<div class="icon"> <!-- div icon -->
+							</div> <!-- end of div icon -->
+							<div class="text-left-about"> <!-- div text-left-about -->
+								<h2>Maecenas imperdiet ultricies dolor cursus accumsan </h2>
+							</div> <!-- end of div text-left-about-4 --> 
+						</div> <!-- end of div text-container-left-about -->
+						<div id="text-container-left-about-5"> <!-- div text-container-left-about-5 -->
+							<div class="icon"> <!-- div icon -->
+							</div> <!-- end of div icon -->
+							<div class="text-left-about"> <!-- div text-left-about -->
+								<h2>In nat sem non lacus euismod aliquet in nec nisl</h2>
+							</div> <!-- end of div text-left-about --> 
+						</div> <!-- end of div text-container-left-about-5 -->
+					</div> <!-- end of div third-left-content-2-about-container -->
+					<div id="fourth-left-content-2-about-container"> <!-- div fourth-left-content-2-about-container -->
+						<p>Lorem ipsum dolor sit amet,consectetur adispicing elit.Mauris tempor , justo sodales fermentum consectetur, libero tellus
+		 		 			fermentum feugiat molestie orci enim sit amet libero.Vivamus non velit in nibh tincidunt leo. Phasellus eget nulla odio.Maecenas 
+		 					 </p>
+					</div> <!-- end of div fourth-left-content-2-about-container -->
+				</div> <!-- end of div left-content-2-about-container -->
+				<div id="right-content-2-about-container"> <!-- div right-content-2-about-container -->
+					<div id="first-right-content-2-about-container"> <!-- div first-right-content-2-about-container -->
+						<h2>Testimonials</h2>
+					</div> <!-- end of div first-right-content-2-about-container -->
+					<div id="second-right-content-2-about-container"> <!-- div second-right-content-2-about-container -->
+						<div class="txt-about1"> <!-- div txt-about1 --> 
+							<h2>William Horner</h2>
+						</div> <!-- end of div txt-about1 -->
+						<div class="txt-about2"> <!-- div txt-about2 -->
+							<h2>Managing Director</h2> 
+						</div> <!-- end of div txt-about2 -->
+						<div class="txt-about3"> <!-- div txt-about3 --> 
+							<h2>Company Name</h2>
+						</div> <!-- end of div txt-about3 -->
+					</div> <!-- end of div second-right-content-2-about-container -->
+					<div id="third-right-content-2-about-container"> <!-- div third-right-content-2-about-container -->
+						<p>Lorem ipsum dolor sit amet,consectetur adispicing elit.Mauris tempor , justo sodales fermentum consectetur, libero tellus
+		 		 			fermentum feugiat  
+		 					 </p>
+					</div> <!-- end of div third-right-content-2-about-container -->
+					<div id="fourth-right-content-2-about-container"> <!-- div fourth-right-content-2-about-container -->
+						<div class="txt-about1"> <!-- div txt-about1 --> 
+							<h2>William Horner</h2>
+						</div> <!-- end of div txt-about1 -->
+						<div class="txt-about2"> <!-- div txt-about2 -->
+							<h2>Managing Director</h2> 
+						</div> <!-- end of div txt-about2 -->
+						<div class="txt-about3"> <!-- div txt-about3 --> 
+							<h2>Company Name</h2>
+						</div> <!-- end of div txt-about3 -->
+					</div> <!-- end of div fourth-right-content-2-about-container -->
+					<div id="fifth-right-content-2-about-container"> <!-- div fifth-right-content-2-about-container -->
+						<p>Lorem ipsum dolor sit amet,consectetur adispicing elit.Mauris tempor , justo sodales fermentum consectetur, libero tellus
+		 		 			fermentum feugiat 
+		 					 </p>
+					</div> <!-- end of div fifth-right-content-2-about-container -->
+				</div> <!-- end of div right-content-2-about-container -->
+			</div> <!-- end of div content-2-about-container -->
+		</div> <!-- end of div content-2-about -->
+	</div> <!-- end of div content-about -->
+	
+<!-- end of CONTENT -->
+
+<?php get_footer(); ?>
